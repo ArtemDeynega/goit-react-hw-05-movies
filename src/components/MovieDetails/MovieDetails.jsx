0e9f7abc5f0879a61fb574382c18b0be30f3b50b
artem <73790146+ArtemDeynega@users.noreply.github.com>
2022-06-01 00:21:47 +0300

    1

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -14,7 +14,6 @@ const MovieDetails = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const history = useRef(location);
-  console.log(history);
 
   const { movieId } = useParams();
 
